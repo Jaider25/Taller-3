b568c455039bfd1aa2151aed31c8cfe40568c375
Jaider25 <ruedacedenojaidereduardo@gmail.com>
2022-03-22 17:28:29 -0500

    subo taller 3

---
+++ js/script.js
@@ -0,0 +1,86 @@
+function cal(){
+    let num1= document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+    let resultado = (num1 / (num2 * num2));
+
+    alert("resultado: "+resultado);
+    document.getElementById('resultado').value=resultado;
+
+    document.getElementById('num1').value="";
+    document.getElementById('num2').value="";
+}
+
+function valu(){
+    let peso =3768;
+    let dolar= document.getElementById('dolar').value;
+    let calculo =parseFloat(peso) * parseFloat(dolar);
+
+    alert("calculo: "+calculo);
+    document.getElementById('calculo').value=calculo;
+
+    document.getElementById('dolar').value="";
+    
+}
+
+function zon(){
+
+    let precio = 44000;
+    let ama = document.getElementById('ama').value;
+    let calculo =parseFloat(precio) * parseFloat(ama);
+
+    alert("calculo: "+calculo);
+    document.getElementById('calculo').value=calculo;
+
+    document.getElementById('ama').value="";
+
+} 
+
+function tify(){
+
+    let precio = 32000;
+    let spot = document.getElementById('spot').value;
+    let calculo =parseFloat(precio) * parseFloat(spot);
+
+    alert("calculo: "+calculo);
+    document.getElementById('calculo').value=calculo;
+
+    document.getElementById('spot').value="";
+
+} 
+
+function leg(){
+
+    let precio = 36000;
+    let lol = document.getElementById('lol').value;
+    let calculo =parseFloat(precio) * parseFloat(lol);
+
+    alert("calculo: "+calculo);
+    document.getElementById('calculo').value=calculo;
+
+    document.getElementById('lol').value="";
+
+} 
+
+function sto(){
+
+    let precio = 28000;
+    let play = document.getElementById('play').value;
+    let calculo =parseFloat(precio) * parseFloat(play);
+
+    alert("calculo: "+calculo);
+    document.getElementById('calculo').value=calculo;
+
+    document.getElementById('play').value="";
+
+} 
+
+
+
+
+
+ 
+
+
+
+
+
